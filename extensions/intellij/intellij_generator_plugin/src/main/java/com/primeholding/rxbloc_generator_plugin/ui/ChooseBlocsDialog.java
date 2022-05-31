c32a7cc392fd9d0defaa31fadb62b48f7b7206bd
Toma Velev <tomavelev@gmail.com>
2022-05-31 17:32:35 +0300

    Generate BloC Tests
    Generate not yet present in the test folder - feature BloC tests - based on Parsing of Folders & Files of the projects lib folder. Updated to meet the ACs requirements.

--- extensions/intellij/intellij_generator_plugin/src/main/java/com/primeholding/rxbloc_generator_plugin/ui/ChooseBlocsDialog.java
+++ extensions/intellij/intellij_generator_plugin/src/main/java/com/primeholding/rxbloc_generator_plugin/ui/ChooseBlocsDialog.java
@@ -9,19 +9,23 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ChooseBlocsDialog extends DialogWrapper {
 
-
     private final List<Bloc> allBlocs;
+    private final List<Bloc> allowedBlocs;
     private final List<Bloc> initiallySelectedBlocs;
 
     public ChooseBlocsDialog(List<Bloc> allBlocs, List<Bloc> initiallySelectedBlocs) {
         super(true);
         this.allBlocs = allBlocs;
         this.initiallySelectedBlocs = initiallySelectedBlocs;
+        this.allowedBlocs = new ArrayList<>(initiallySelectedBlocs);
         setTitle("Select BloCs");
+        setOKButtonText("Create");
+        setModal(true);
         init();
     }
 
@@ -34,43 +38,44 @@ public class ChooseBlocsDialog extends DialogWrapper {
         list.setCellRenderer(new CheckListRenderer());
         list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 
-        list.
-                addMouseListener(new MouseAdapter() {
-                                     @Override
-                                     public void mousePressed(MouseEvent e) {
-                                         int index = list.locationToIndex(e.getPoint());
-                                         if (index != -1) {
-                                             Bloc value = allBlocs.get(index);
-                                             boolean newValue = initiallySelectedBlocs.contains(value);
-                                             if (newValue) {
-                                                 //rethink this, as it uses a side effect of - pass value by reference (the select blocs list).
-                                                 if (initiallySelectedBlocs.contains(value)) {
-                                                     initiallySelectedBlocs.remove(value);
-                                                 } else {
-                                                     initiallySelectedBlocs.add(value);
-                                                 }
-                                             } else {
-                                                 initiallySelectedBlocs.add(value);
-                                             }
+        list.addMouseListener(new MouseAdapter() {
+                                  @Override
+                                  public void mousePressed(MouseEvent e) {
+                                      int index = list.locationToIndex(e.getPoint());
+                                      if (index != -1) {
+                                          Bloc value = allBlocs.get(index);
+                                          if (allowedBlocs.contains(value)) {
+                                              boolean newValue = initiallySelectedBlocs.contains(value);
+                                              if (newValue) {
+                                                  // rethink this, as it uses a side effect of - pass value by reference (the
+                                                  // select blocs list).
+                                                  if (initiallySelectedBlocs.contains(value)) {
+                                                      initiallySelectedBlocs.remove(value);
+                                                  } else {
+                                                      initiallySelectedBlocs.add(value);
+                                                  }
+                                              } else {
+                                                  initiallySelectedBlocs.add(value);
+                                              }
 
+                                              repaint();
+                                          }
+                                      }
+                                  }
+                              }
 
-                                             repaint();
-                                         }
-                                     }
-                                 }
-
-                );
+        );
         dialogPanel.add(new JScrollPane(list), BorderLayout.CENTER);
-
         return dialogPanel;
     }
-
 
     class CheckListRenderer extends JCheckBox implements ListCellRenderer<Bloc> {
         public Component getListCellRendererComponent(JList list, Bloc value,
                                                       int index, boolean isSelected, boolean hasFocus) {
-            setSelected(initiallySelectedBlocs.contains(value));
+            boolean checked = initiallySelectedBlocs.contains(value);
+            setSelected(checked);
             setFont(list.getFont());
+            setEnabled(allowedBlocs.contains(value));
             setBackground(list.getBackground());
             setForeground(list.getForeground());
             setText(value.getFileName());
@@ -78,6 +83,4 @@ public class ChooseBlocsDialog extends DialogWrapper {
         }
     }
 
-
 }
-
